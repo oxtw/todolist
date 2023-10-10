f27408e5ebaacac1b694bdc026a09631e71707eb
oxtw <weigertmiguel@hotmail.com>
2023-10-09 22:44:35 -0300

    Projeto criado

---
+++ src/test/java/br/com/migueljava/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.migueljava.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
